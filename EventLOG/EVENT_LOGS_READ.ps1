18662f782728626a219ee7366cac6dcd3909d7ad
EvgeniyShell <37295805+EvgeniyShell@users.noreply.github.com>
2018-06-04 18:10:11 +1100

    Upload

---
+++ EventLOG/EVENT_LOGS_READ.ps1
@@ -0,0 +1,12 @@
+﻿Get-Eventlog Security -InstanceId 4624 -ErrorAction Ignore |
+   Select TimeGenerated,ReplacementStrings |
+   % {
+     New-Object PSObject -Property @{
+     UserName = $_.ReplacementStrings[5]
+     IPAddress = $_.ReplacementStrings[18]
+     Workstation = $_.ReplacementStrings[11]
+     AuthenticationName = $_.ReplacementStrings[10]
+     ID = $_.ReplacementStrings[3]
+     Date = $_.TimeGenerated
+    }
+   } | ?{$_.UserName -notlike "*PSO-EX*"} | Out-GridView
